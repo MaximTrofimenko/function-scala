dbd38c5e6600b1745645493743bc474c132bfbc0
Maxim Trofimenko <maxim.trofimenko@corp.codeinside.ru>
2024-12-04 13:53:52 +0300

    upgrade BeautifulFunc

--- src/main/scala/BeautifulFunc.scala
+++ src/main/scala/BeautifulFunc.scala
@@ -8,12 +8,24 @@ object BeautifulFunc {
 
     val words = List("Samsung", "Kotlin", "fire", "deal", "count")
 
-    val result = rankedWords(w => score(w) + bonus(w), words)
+    val result = rankedWords(w => score(w) + bonus(w) - fine(w), words)
+    val result2 = rankedWords(score, words) //мы можем просто указать функцию score если она имеет нужную сигнатуру, без лишних кавычек
+
+    println(result2)
   }
 
-  private def rankedWords(wordScore: String => Int, words: List[String]) = words.sortBy(wordScore).reverse
+  private def rankedWords(wordScore: String => Int, words: List[String]): List[String] = {
+    words.sortBy(wordScore).reverse
+  }
 
-  private def score(word: String) = word.replaceAll("a", "").length
+  private def score(word: String): Int = {
+    word.replaceAll("a", "").length
+  }
 
-  private def bonus(word: String) = if (word.contains("c")) 5 else 0
+  private def bonus(word: String): Int = {
+    if (word.contains("c")) 5 else 0
+  }
+
+  private def fine(word: String) = if (word.contains("s")) 7 else 0
+
 }
